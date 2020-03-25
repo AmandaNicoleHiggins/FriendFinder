884ab9e1c9a32d4a85ee988cf8be89bba840fd6b
Amanda Higgins <amanda.n.higgins@gmail.com>
2020-03-25 18:48:13 -0400

    adding friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,101 @@
-{
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
+var arrayOfFriends = [
+    {
+    friendName: "Rachel",
+    friendImage: "https://giphy.com/gifs/friends-flirting-flirt-SWPwSP6poA1nG",
+    scores: [
+        3,
+        2,
+        2,
+        3,
+        2,
         1,
         2,
-        5,
+        1,
         4,
-        1
-      ]
-  }
+        5
+    ]
+},
+
+{
+    friendName: "Ross",
+    friendImage: "https://giphy.com/gifs/Friends-season-5-episode-7-friends-tv-iHskdY9SMLFZuQ2u5c",
+    scores: [
+        3,
+        2,
+        2,
+        3,
+        2,
+        1,
+        2,
+        1,
+        4,
+        5
+    ]
+},
+
+{
+    friendName: "Phoebe",
+    friendImage: "https://giphy.com/gifs/dance-dancing-friends-JGH2CwqB4oqGc",
+    scores: [
+        3,
+        2,
+        2,
+        3,
+        2,
+        1,
+        2,
+        1,
+        4,
+        5
+    ]
+}, {
+friendName: "Monica",
+friendImage: "https://giphy.com/gifs/dance-dancing-friends-JGH2CwqB4oqGc",
+scores: [
+    3,
+    2,
+    2,
+    3,
+    2,
+    1,
+    2,
+    1,
+    4,
+    5
+]
+}, {
+friendName: "Chandler",
+friendImage: "https://giphy.com/gifs/ross-gellar-ZQjUvxPYUQ21q",
+scores: [
+    3,
+    2,
+    2,
+    3,
+    2,
+    1,
+    2,
+    1,
+    4,
+    5
+]
+}, {
+friendName: "Joey",
+friendImage: "https://giphy.com/gifs/pyEDsTDy2aIR5SXkBD",
+scores: [
+    3,
+    2,
+    2,
+    3,
+    2,
+    1,
+    2,
+    1,
+    4,
+    5
+]
+}
+];
+
+// export the array to amke it accessible to other files using require
+module.exports = arrayOfFriends;
+console.log(arrayOfFriends);
